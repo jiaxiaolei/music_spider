13bfda6654e1014bfc0228790bfd0c115bb70baf
jiaxiaolei <jiaxiaolei@syswin.com>
2019-02-20 14:32:11 +0800

    featïï: add download

--- tutorial/tutorial/spiders/music_mp3_spider.py
+++ tutorial/tutorial/spiders/music_mp3_spider.py
@@ -16,22 +16,23 @@ class QuotesSpider(scrapy.Spider):
         print("------mp3 href: ", href)
         item = dict(mp3_url=href)
         insert_music_item(item)
+
         return
 
-        for item in response.css('div.listboxmain'):
-            for i in item.css('a.listboxrow'):
-                
-                
-                href =  i.css('a.listboxrow').attrib['href']
+        #for item in response.css('div.listboxmain'):
+        #    for i in item.css('a.listboxrow'):
+        #        
+        #        
+        #        href =  i.css('a.listboxrow').attrib['href']
 
-                span =  i.css('span::text').extract()
-                if len(span) != 3:
-                    continue
-                 
-                yield {
-                    'artname': i.css('span::text').extract_first(),
-                    'href': href,
-                    'title': i.css('span::text').extract()[1],
-                    'date': i.css('span::text').extract()[2] ,
-                }
+        #        span =  i.css('span::text').extract()
+        #        if len(span) != 3:
+        #            continue
+        #         
+        #        yield {
+        #            'artname': i.css('span::text').extract_first(),
+        #            'href': href,
+        #            'title': i.css('span::text').extract()[1],
+        #            'date': i.css('span::text').extract()[2] ,
+        #        }
 
---
+++ tutorial/tutorial/spiders/test_download.py
@@ -0,0 +1,62 @@
+
+import os
+
+import requests
+
+def mkdir(path):
+    isExists = os.path.exists(path)
+    if not isExists:
+        os.makedirs(path)
+
+#url = 'https://www.8notes.com/school/mp32/piano/chopin_prelude_28_4.mp3'
+
+url = 'https://www.8notes.com/school/png/piano/chopin_prelude_28_4001.png'
+
+file_name = url.split('/')[-1]
+
+response = requests.get(url)
+
+my_pdf_path = '/root/jia_scrapy/tutorial/tutorial/spiders/pdf'
+my_mp3_path = '/root/jia_scrapy/tutorial/tutorial/spiders/mp3'
+
+path = file_path + '/%s' % file_name 
+
+print '----file---', file_name 
+
+mkdir(file_path)
+
+with open(path, 'wb') as f:
+    f.write(response.content)
+
+print 'end'
+
+
+def download_pdf(url, file_path=None, file_name=None):
+
+    if not file_path:
+        file_path = my_pdf_path 
+    
+    if not file_name:
+        file_name = url.split('/')[-1]
+
+    mkdir(file_path)
+    
+    path = file_path + '/%s' % file_name 
+    with open(path, 'wb') as f:
+        f.write(response.content)
+
+def download_mp3(url, file_path=None, file_name=None):
+
+    if not file_path:
+        file_path = my_mp3_path 
+    
+    if not file_name:
+        file_name = url.split('/')[-1]
+
+    mkdir(file_path)
+    
+    path = file_path + '/%s' % file_name 
+    with open(path, 'wb') as f:
+        f.write(response.content)
+
+
